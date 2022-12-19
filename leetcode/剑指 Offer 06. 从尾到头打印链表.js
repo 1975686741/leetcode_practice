32eca03d96b7e1c864c8b3488863a9168dca59a5
Summer <113973444+1975686741@users.noreply.github.com>
2022-12-19 23:44:28 +0800

    Create 剑指 Offer 06. 从尾到头打印链表.js

---
+++ leetcode/剑指 Offer 06. 从尾到头打印链表.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var reversePrint = function(head) {
+    if(!head) return []
+    let arr = [];
+    while(head){
+        arr.push(head.val);
+        head = head.next;
+    }
+    return arr.reverse()
+};
