7be049eb19b4d48ac55386ae666db77b7314a6e1
Summer <113973444+1975686741@users.noreply.github.com>
2023-02-08 22:15:45 +0800

    Create 剑指 Offer 65. 不用加减乘除做加法.js

---
+++ leetcode/剑指 Offer 65. 不用加减乘除做加法.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var add = function(a, b) {
+    if(b === 0) {
+        return a
+    }
+    return add(a ^ b, (a&b) << 1)
+};
