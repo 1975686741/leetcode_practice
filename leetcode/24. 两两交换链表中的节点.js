cec660578d15c016533ab307199abdf84c6e3676
Summer <113973444+1975686741@users.noreply.github.com>
2023-06-23 23:15:33 +0800

    Create 24. 两两交换链表中的节点.js

---
+++ leetcode/24. 两两交换链表中的节点.js
@@ -0,0 +1,10 @@
+/ *给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。*/ 
+var swapPairs = function(head) {
+    if (head === null|| head.next === null) {
+        return head;
+    }
+    const newHead = head.next;
+    head.next = swapPairs(newHead.next);
+    newHead.next = head;
+    return newHead;
+};
